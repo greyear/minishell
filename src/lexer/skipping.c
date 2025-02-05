77394a8251394bb1d31bacefdbd8c24050af04e0
azinchen <azinchen@student.hive.fi>
2025-02-05 20:02:06 +0200

    merging of some tokens in progress

--- src/lexer/skipping.c
+++ src/lexer/skipping.c
@@ -5,13 +5,10 @@ void	skip_special_tokens(char *str, size_t *i, t_token_type type)
 {
 	if (!str || !i)
 		return;
-	while (str[*i])
-	{
-		if (type == HEREDOC || type == APPEND)
-			(*i) += 2;
-		else if (type != WORD && type != SPACE)
-			(*i)++;
-	}
+	if (str[*i + 1] && (type == HEREDOC || type == APPEND))
+		(*i) += 2;
+	else if (str[*i] && (type != WORD && type != SPACE))
+		(*i)++;
 }
 
 void	skip_whitespaces(char *str, size_t *i)
