722cda7cc64548c9967f0668126b9a9ed8c7ef3d
Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
2025-02-10 20:21:46 +0200

    [DIVIDED]: to short functions and multiple files

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -21,24 +21,45 @@
 	bash: cd: HOME not set
 	if export OLDPWD= and cd -
 	print newline and then change OLDPWD value to same as PWD’s value
-	if export OLDPWD=, export PWD=, and cd -
+	if export OLDPWD=, export PWD=, and cd ..
 	print newline and change PWD value to the actual directory path we are in
-	if cd - is used again, prints a newline, now when PWD has a value it changes OLDPWD value to same as PWD’s value
-
-	cd .. still works even if PWD=invalidpath and OLDPWD=invalidpath
+	if cd .. is used again, prints a newline, now when PWD has a value it changes OLDPWD value to same as PWD’s value
 
 */
 
 #include "seela.h"
 
-static void	print_cd_error(char *target_dir)
+static char	*get_cd_target(t_ms *ms, char **args)
 {
-	ft_putstr_fd("bash: cd: ", 2);
-	if (target_dir && *target_dir)
-		ft_putstr_fd(target_dir, 2);
-	else
-		ft_putstr_fd(" ", 2);
-	ft_putstr_fd(": No such file or directory\n", 2);
+	if (!args[1])
+		return (get_home_directory(ms));
+	if (ft_strcmp(args[1], "-") == 0)
+		return (get_oldpwd_directory(ms));
+	if (ft_strcmp(args[1], "..") == 0)
+		return (get_parent_directory());
+	if (args[1][0] != '/')
+	{
+		char cwd[1024];
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+			return (NULL);
+		return (build_relative_path(args[1], cwd));
+	}
+	return (ft_strdup(args[1]));
+}
+
+static void	update_cd_env(t_ms *ms)
+{
+	char cwd[1024];
+
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	{
+		printf("cd: getcwd failed");
+		ms->exit_status = 1;
+		return;
+	}
+	update_env_var(ms, "OLDPWD=", get_env_value("PWD", ms->envp));
+	update_env_var(ms, "PWD=", cwd);
+	ms->exit_status = 0;
 }
 
 static void	add_to_env_end(t_ms *ms, char *new_env_entry, int i)
@@ -47,24 +68,22 @@ static void	add_to_env_end(t_ms *ms, char *new_env_entry, int i)
 	char	**new_envp;
 
 	j = 0;
-	 // Key not found, need to expand envp
-    new_envp = malloc((i + 2) * sizeof(char *)); // +2 for new var & NULL
-    if (!new_envp)
-    {
-        free(new_env_entry);
-        return;
-    }
-    // Copy old envp
-    while (j < i)
+	new_envp = malloc((i + 2) * sizeof(char *));
+	if (!new_envp)
 	{
-        new_envp[j] = ms->envp[j];
+		free(new_env_entry);
+		return;
+	}
+	while (j < i)
+	{
+		new_envp[j] = ms->envp[j];
 		j++;
 	}
-    new_envp[i] = new_env_entry;
-    new_envp[i + 1] = NULL;
-
-    free(ms->envp); // Free old envp array
-    ms->envp = new_envp;
+	new_envp[i] = new_env_entry;
+	new_envp[i + 1] = NULL;
+	ft_free_array(ms->envp);
+	ms->envp = copy_map(new_envp);
+	free(new_envp);
 }
 
 static void	update_env_var(t_ms *ms, char *key, char *new_value)
@@ -93,106 +112,6 @@ static void	update_env_var(t_ms *ms, char *key, char *new_value)
 	add_to_env_end(ms, new_env_entry, i);
 }
 
-static char	*get_cd_target(t_ms *ms, char **args)
-{
-	char	*target;
-	char	*parent_dir;
-	char	*current_dir;
-	char	cwd[1024]
-
-	if (!args[1])
-	{
-		target = get_env_value("HOME", ms->envp);
-		if (!target) //if no HOME
-		{
-			ft_putstr_fd("bash: cd: HOME not set\n", 2);
-			ms->exit_status = 1;
-			return (NULL);
-		}
-		if (*target == '\0') //if HOME=
-		{
-			ms->exit_status = 1;
-			return (NULL);
-		}
-		if (access(target, F_OK) != 0)
-		{
-			print_cd_error(target);
-			ms->exit_status = 1;
-			return (NULL);
-		}
-	}
-	else if (ft_strcmp(args[1], "-") == 0)
-	{
-		target = get_env_value("OLDPWD", ms->envp);
-		if (!target || *target == '\0')
-		{
-			ft_putstr_fd("\n", 1);
-			current_pwd = get_env_value("PWD", ms->envp);
-			if (!current_pwd || *current_pwd == '\0')
-			{
-				if (getcwd(cwd, sizeof(cwd)))
-					update_env_var(ms, "PWD=", cwd);
-				return (NULL);
-			}
-			update_env_var(ms, "OLDPWD=", current_pwd);
-			return (current_pwd);
-		}
-		ft_putendl_fd(target, 1);
-	}
-	else if (ft_strcmp(args[1], "..") == 0) // Handle ..
-    {
-        if (getcwd(cwd, sizeof(cwd)) == NULL) {
-            perror("cd: getcwd failed");
-            ms->exit_status = 1;
-            return (NULL);
-        }
-        parent_dir = ft_strrchr(cwd, '/');
-        if (parent_dir && parent_dir != cwd)
-		{
-            *parent_dir = '\0';  // Trim the last part of the path to go to the parent
-            target = cwd;
-        }
-		else
-            target = "/";
-    }
-	else // Handle other paths (either absolute or relative)
-    {
-        if (getcwd(cwd, sizeof(cwd)) == NULL)
-        {
-            perror("cd: getcwd failed");
-            ms->exit_status = 1;
-            return (NULL);
-        }
-		// If the target is relative, construct the full path
-        if (args[1][0] != '/') // Check if it is relative
-        {
-            target = ft_strjoin(cwd, "/");
-            target = ft_strjoin(target, args[1]);
-        }
-        else
-        {
-            // It's an absolute path
-            target = args[1];
-        }
-    }
-	return (target);
-}
-
-static void	update_cd_env(t_ms *ms)
-{
-	char	cwd[1024];
-
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		printf("cd: getcwd failed");
-		ms->exit_status = 1;
-		return;
-	}
-	update_env_var(ms, "OLDPWD=", get_env_value("PWD", ms->envp));
-	update_env_var(ms, "PWD=", cwd);
-	ms->exit_status = 0;
-}
-
 void	handle_cd(t_ms *ms, char **args)
 {
 	char	*target_dir;
@@ -201,13 +120,15 @@ void	handle_cd(t_ms *ms, char **args)
 	if (ft_strcmp(args[0], "cd") != 0)
 		return;
 	target_dir = get_cd_target(ms, args);
-	if (ms->exit_status == 1)
+	if (ms->exit_status == 1 || !target_dir)
 		return;
 	if (chdir(target_dir) == -1)
 	{
 		print_cd_error(target_dir);
+		free(target_dir);
 		ms->exit_status = 1;
 		return;
 	}
+	free(target_dir);
 	update_cd_env(ms);
 }
