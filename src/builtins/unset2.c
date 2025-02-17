7c08d81d349f1775f5236d6149c983794fc3ebc8
Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
2025-02-17 14:36:29 +0200

    safety push

--- src/builtins/unset2.c
+++ src/builtins/unset2.c
@@ -1,33 +1,5 @@
 #include "../../include/minishell.h"
 
-static int	should_skip(char *env, char *name, int len, int flag)
-{
-	if (ft_strncmp(name, env, len) != 0)
-		return (0);
-	if (flag == 0 && (env[len] && env[len] == '='))
-		return (1);
-	if (flag == 1 && env[len]
-		&& (env[len] == '=' || env[len] == '\0'))
-		return (1);
-	return (0);
-}
-
-static char	**allocate_temp_env(char **env)
-{
-	char	**temp;
-	int	i;
-
-	i = 0;
-	while (env[i])
-		i++;
-	if (i == 0)
-		return (NULL);
-	temp = malloc(sizeof(char *) * i);
-	if (!temp)
-		return (NULL);
-	return (temp);
-}
-
 static char	**filter_env_entries(char **env, char *name, int len, int flag)
 {
 	char	**temp;
