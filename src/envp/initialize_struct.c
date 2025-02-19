700777dd834cb6871190d9b31cf87fc023260cad
SeelaSalorinta <44392065+SeelaSalorinta@users.noreply.github.com>
2025-02-19 12:30:56 +0200

    Update initialize_struct.c

--- src/envp/initialize_struct.c
+++ src/envp/initialize_struct.c
@@ -1,48 +1,5 @@
 #include "../../include/minishell.h"
 
-
-char	**make_args(char *cmd, char *name)
-{
-	char	**args;
-
-	args = malloc(sizeof(char*) * 3);
-	if (!args)
-		return (NULL);
-	args[0] = ft_strdup(cmd);
-	if (!args[0])
-	{
-		ft_free_map(args);
-		return (NULL);
-	}
-	args[1] = ft_strdup(name);
-	if (!args[1])
-	{
-		ft_free_map(args);
-		return (NULL);
-	}	
-	args[2] = NULL;
-	return (args);
-}
-
-//We need to remove OLDPWD in the beginning like in bash!, and add it to exported without an value!
-
-int	handle_oldpwd(t_ms *ms)
-{
-	char	**args;
-
-	args = make_args("unset", "OLDPWD");
-	if (!args)
-		return (1);
-	handle_unset(args, ms);
-	ft_free_map(args);
-	args = make_args("export", "OLDPWD");
-	if (!args)
-		return (1);
-	handle_export(args, ms);
-	ft_free_map(args);
-	return (0);
-}
-
 t_ms	*initialize_struct(char **envp)
 {
 	t_ms *ms;
@@ -53,8 +10,5 @@ t_ms	*initialize_struct(char **envp)
 	ms->exit_status = 0;
 	ms->envp = copy_map(envp);
 	ms->exported = copy_map(envp);
-	ms->oldpwd_check = 1;
-	if (handle_oldpwd(ms) == 1)
-		return (NULL);
 	return (ms);
 }
