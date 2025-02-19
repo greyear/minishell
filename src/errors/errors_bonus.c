73b7240945b229fe986e378cead0cb46fe9118c4
azinchen <azinchen@student.hive.fi>
2025-02-19 02:11:46 +0200

    redirection functions rewrote, started handling ambiguous redirect problem

--- src/errors/errors_bonus.c
+++ src/errors/errors_bonus.c
@@ -1,7 +1,7 @@
 
 #include "../../include/minishell.h"
 
-void	execve_fail(char **path, char **cmd_split, t_pipex **p)
+/*void	execve_fail(char **path, char **cmd_split, t_pipex **p)
 {
 	perror(cmd_split[0]);
 	free(*path);
@@ -9,9 +9,9 @@ void	execve_fail(char **path, char **cmd_split, t_pipex **p)
 	//clean, save error code and exit
 	if (p)
 		exit(CMD_EXEC);
-}
+}*/
 
-void	cmd_error(char *reason, char *cmd, int exit_code, t_pipex **p)
+/*void	cmd_error(char *reason, char *cmd, int exit_code, t_pipex **p)
 {
 	if (ft_printf(2, "%s %s\n", reason, cmd) == -1)
 	{
@@ -21,7 +21,7 @@ void	cmd_error(char *reason, char *cmd, int exit_code, t_pipex **p)
 	//clean, save error code and exit
 	if (p)
 		exit(exit_code);
-}
+}*/
 
 void	error_exit_code(char *reason, int exit_code)
 {
@@ -29,11 +29,19 @@ void	error_exit_code(char *reason, int exit_code)
 	exit(exit_code);
 }
 
-void	error_clean_exit_code(char *reason, int exit_code, t_pipex **p)
+void	print_file_error(char *file, t_print reason)
 {
-	//clean, save error code and exit
-	if (p)
-		perror(reason);
-	exit(exit_code);
+	ft_putstr_fd(OWN_ERR_MSG, STDERR_FILENO);
+	if (reason == HERED_ERR)
+	{
+		//put msg
+	}
+	else
+		ft_putstr_fd(file, STDERR_FILENO);
+	if (reason == NO_FILE)
+		ft_putendl_fd(": No such file or directory", STDERR_FILENO);
+	if (reason == PERM_DEN)
+		ft_putendl_fd(": Permission denied", STDERR_FILENO);
+	if (reason == AMBIG)
+		ft_putendl_fd(": ambiguous redirect", STDERR_FILENO);
 }
-
