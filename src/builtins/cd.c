0c257fd164045d4fac46b11cb11e39b7919d5aad
Seela Salorinta <ssalorin@c1r5p15.hive.fi>
2025-02-21 18:11:23 +0200

    error handling for multiple arguments

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -100,20 +100,20 @@ void	update_env_var(t_ms *ms, char *key, char *new_value)
 	}
 }
 
-/*static void	add_oldpwd_first_time(t_ms *ms, char *pwd_before)
+static int	cd_error(t_ms *ms, char **args)
 {
-	char	**args;
-	char	*oldpwd;
-	
-	oldpwd = ft_strjoin("OLDPWD=", pwd_before);
-	if (!oldpwd)
-		return;
-	args = make_args("export", oldpwd);
-	if (!args)
-		return;
-	handle_export(args, ms);
-	ms->oldpwd_check = 0;
-}*/
+	if (!args || !*args)
+		return (1);
+	if (ft_strcmp(args[0], "cd") != 0)
+		return (1);
+	if (args[2])
+	{
+		ft_putstr_fd("bash: cd: too many arguments\n", 2);
+		ms->exit_status = 1;
+		return (1);
+	}
+	return (0);
+}
 
 
 void	handle_cd(t_ms *ms, char **args)
@@ -122,7 +122,7 @@ void	handle_cd(t_ms *ms, char **args)
 	char	cwd[1024];
 
 	ms->exit_status = 0;
-	if (ft_strcmp(args[0], "cd") != 0)
+	if (cd_error(ms, args))
 		return;
 	target_dir = get_cd_target(ms, args);
 	if (ms->exit_status == 1 || !target_dir)
@@ -142,6 +142,4 @@ void	handle_cd(t_ms *ms, char **args)
 	}
 	free(target_dir);
 	update_cd_env(ms, cwd);
-	//if (ms->oldpwd_check == 1)
-	//	add_oldpwd_first_time(ms, cwd);
 }
