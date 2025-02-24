51f6a5b5f6049add3882d70be6e04f10ae44a4dd
azinchen <azinchen@student.hive.fi>
2025-02-24 15:29:14 +0200

    added exit status changing if files don't exist in redirections and check in the exec part

--- src/parser/cmd_creation.c
+++ src/parser/cmd_creation.c
@@ -50,7 +50,7 @@ int	put_cmg_args(t_cmd *cmd, t_token *start, t_token *end)
 	return (0);
 }
 
-void	redir_in_block(t_block *block, t_cmd *cmd) //, t_ms *ms)
+void	redir_in_block(t_block *block, t_cmd *cmd, t_ms *ms)
 {
 	t_token	*cur;
 
@@ -63,7 +63,11 @@ void	redir_in_block(t_block *block, t_cmd *cmd) //, t_ms *ms)
 				put_infile_fd(cur, cmd);
 			else if (cur->type == OUT || cur->type == APPEND)
 				put_outfile_fd(cur, cmd);
-			//if smth failed here (cmd->infile || cmd->outfile == NO_FD) we save exit value for the process
+			if (cmd->infile == NO_FD || cmd->outfile == NO_FD)
+			{
+				ms->exit_status = 1;
+				return ;
+			} //if smth failed here we save exit value for the process
 		}
 		cur = cur->next;
 	}
@@ -86,7 +90,7 @@ t_cmd	*create_new_cmd(t_block *block, int num, t_ms *ms)
 	if (put_cmg_args(new, block->start, block->end))
 		return (clean_cmd(new));
 	new->name = new->args[0];
-	redir_in_block(block, new);
+	redir_in_block(block, new, ms);
 	return (new);
 }
 
