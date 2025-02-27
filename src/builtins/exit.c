7d68393b7f0d42087966645166c8b3bd4a7c989a
azinchen <azinchen@student.hive.fi>
2025-02-27 19:57:28 +0200

    0 leaks after exit in case I tried, but need to handle leaks after each cmd input

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -35,18 +35,18 @@ static void	exit_shell(long long exit_nbr, int error, char **array, t_ms *ms)
 		ft_putstr_fd("bash: exit: ", 2);
 		ft_putstr_fd(array[1], 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
-		/*clean_cmd_list(&(ms->cmds));
-		clean_struct(ms);*/
+		clean_cmd_list(&(ms->cmds));
+		clean_struct(ms);
 		exit(2);
 	}
 	if (exit_nbr > 255 || exit_nbr < 0)
 	{
-		/*clean_cmd_list(&(ms->cmds));
-		clean_struct(ms);*/
+		clean_cmd_list(&(ms->cmds));
+		clean_struct(ms);
 		exit(exit_nbr % 256);
 	}
-	/*clean_cmd_list(&(ms->cmds));
-	clean_struct(ms);*/
+	clean_cmd_list(&(ms->cmds));
+	clean_struct(ms);
 	exit(exit_nbr);
 }
 
@@ -69,24 +69,16 @@ void	check_exit(char **array, t_ms *ms)
 			ms->exit_status = 1;
 			return;
 		}
-		clean_token_list(&(ms->tokens));
-		clean_block_list(&(ms->blocks));
-		//clean_cmd_list(&(ms->cmds));
 		//free_struct(ms);
-		clean_struct(ms);
+		//clean_struct(ms);
 		exit_nbr = ft_strtoll(array[1], &error);	// If non-numeric or out of range, print error and exit(2)
 		exit_shell(exit_nbr, error, array, ms);
 	}
 	exit_code = ms->exit_status;
 	//free_struct(ms); Changed it as it gave a leak, i can explain!
-	clean_token_list(&(ms->tokens));
-	clean_block_list(&(ms->blocks));
 	clean_cmd_list(&(ms->cmds));
-	//printf("cleaning inside exit\n");
-	//clean_cmd_list(&(ms->cmds));
-	//clean_cmd_list(&(ms->cmds));
 	clean_struct(ms);
 	exit(exit_code);
 }
-
+ 
 //check more the case when I call 1- echo, 2-exit 42 - leaks in exit
