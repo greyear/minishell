c57cf7f62d6d5d01cdb7e26e523a252cdfbbe881
Seela Salorinta <44392065+SeelaSalorinta@users.noreply.github.com>
2025-03-05 17:58:29 +0200

    Seela, Restructuring execution and error handling  (#38)
    * addedcheck for one plus/minus ahead of number
    
    * nothing
    
    * shortened error functions
    
    * shortened functions
    
    * shortened functions
    
    * divided functions to shorter ones
    
    * removed empty files
    
    * removed empty files
    
    * return exit_code correctly now
    
    * fixed error with just using cd
    
    * fixed error with too many args with cd
    
    * fixed some tester cases
    
    * error handling if PATH empty
    
    * error handling if file exists but no permissions
    
    * file handling
    
    * file handling
    
    * cd with too many args
    
    * functions cleaned adn shortened
    
    * restructuring execution
    
    * restructuring execution
    
    ---------
    
    Co-authored-by: Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
    Co-authored-by: Anya Zinchenko <63639724+greyear@users.noreply.github.com>

--- src/errors/errors_bonus.c
+++ src/errors/errors_bonus.c
@@ -52,55 +52,57 @@ void	print_file_error(char *file, t_print reason)
 
 void	print_cmd_error(char *cmd, int c)
 {
-	ft_putstr_fd(OWN_ERR_MSG, 2);
-	ft_putstr_fd(cmd, 2);
-	if (cmd[0] == '/' || c == 2)
-		ft_putstr_fd(": No such file or directory\n", 2);
-	else if (cmd[0] == '.' || c == 1)
-		ft_putstr_fd(": Permission denied\n", 2);
-	else
-		ft_putstr_fd(": command not found\n", 2);
+	ft_putstr_fd(OWN_ERR_MSG, STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	if (c == IS_DIR)
+		ft_putstr_fd(": is a directory\n", STDERR_FILENO);
+	else if (c == NO_FILE_OR_DIR)
+		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
+	else if (c == PERM_DEN)
+		ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
+	else if (c == NO_CMD)
+		ft_putstr_fd(": command not found\n", STDERR_FILENO);
 }
 
 void	print_unset_error(char **args, int i, t_ms *ms)
 {
-	ft_putstr_fd(OWN_ERR_MSG, 2);
-	ft_putstr_fd("unset: '", 2);
-	ft_putstr_fd(args[i], 2);
-	ft_putstr_fd("': not a valid indentifier\n", 2);
+	ft_putstr_fd(OWN_ERR_MSG, STDERR_FILENO);
+	ft_putstr_fd("unset: '", STDERR_FILENO);
+	ft_putstr_fd(args[i], STDERR_FILENO);
+	ft_putstr_fd("': not a valid indentifier\n", STDERR_FILENO);
 	ms->exit_status = 1;
 }
 
 void	print_export_error(t_ms *ms, char *arg)
 {
-    ft_putstr_fd(OWN_ERR_MSG, 2);
-	ft_putstr_fd("export: '", 2);
-	ft_putstr_fd(arg, 2);
-	ft_putstr_fd("': not a valid identifier\n", 2);
+    ft_putstr_fd(OWN_ERR_MSG, STDERR_FILENO);
+	ft_putstr_fd("export: '", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
 	ms->exit_status = 1;
 }
 
 void	print_cd_error(char *target_dir)
 {
-	ft_putstr_fd(OWN_ERR_MSG, 2);
-	ft_putstr_fd("cd: ", 2);
+	ft_putstr_fd(OWN_ERR_MSG, STDERR_FILENO);
+	ft_putstr_fd("cd: ", STDERR_FILENO);
 	if (target_dir && *target_dir)
-		ft_putstr_fd(target_dir, 2);
+		ft_putstr_fd(target_dir, STDERR_FILENO);
 	else
-		ft_putstr_fd(" ", 2);
-	ft_putstr_fd(": No such file or directory\n", 2);
+		ft_putstr_fd(" ", STDERR_FILENO);
+	ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
 }
 
 void	print_numeric_error(char **array)
 {
-    ft_putstr_fd(OWN_ERR_MSG, 2);
-    ft_putstr_fd("exit: ", 2);
-    ft_putstr_fd(array[1], 2);
-    ft_putstr_fd(": numeric argument required\n", 2);
+    ft_putstr_fd(OWN_ERR_MSG, STDERR_FILENO);
+    ft_putstr_fd("exit: ", STDERR_FILENO);
+    ft_putstr_fd(array[1], STDERR_FILENO);
+    ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
 }
 
 void	print_too_many_args_error(void)
 {
-    ft_putstr_fd(OWN_ERR_MSG, 2);
-    ft_putstr_fd("exit: too many arguments\n", 2);
+    ft_putstr_fd(OWN_ERR_MSG, STDERR_FILENO);
+    ft_putstr_fd("exit: too many arguments\n", STDERR_FILENO);
 }
