1e860b755ff172a419b5633ae26e1c2dce00116b
Seela Salorinta <44392065+SeelaSalorinta@users.noreply.github.com>
2025-03-06 20:34:16 +0200

    Seela, closing fd's after execution (#41)
    * addedcheck for one plus/minus ahead of number
    
    * nothing
    
    * shortened error functions
    
    * shortened functions
    
    * shortened functions
    
    * divided functions to shorter ones
    
    * removed empty files
    
    * removed empty files
    
    * return exit_code correctly now
    
    * fixed error with just using cd
    
    * fixed error with too many args with cd
    
    * fixed some tester cases
    
    * error handling if PATH empty
    
    * error handling if file exists but no permissions
    
    * file handling
    
    * file handling
    
    * cd with too many args
    
    * functions cleaned adn shortened
    
    * restructuring execution
    
    * restructuring execution
    
    * pulling
    
    * closing fd in parent after execution
    
    * closing fd in parent after execution
    
    * closing fds after execution
    
    ---------
    
    Co-authored-by: Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
    Co-authored-by: Anya Zinchenko <63639724+greyear@users.noreply.github.com>

--- src/cleaners/arr_cleaning.c
+++ src/cleaners/arr_cleaning.c
@@ -48,3 +48,22 @@ void cleanup_heredocs(char **filenames)
     }
     free(filenames);  // Free the array of filenames
 }
+
+void	close_fds(t_cmd *cmd)
+{
+	t_cmd	*cur;
+
+	cur = cmd;
+	while (cur)
+	{
+		if (cur->infile != DEF && cur->infile != NO_FD)
+		{
+			close(cur->infile);
+		}
+		if (cur->outfile != DEF && cur->outfile != NO_FD)
+		{
+			close(cur->outfile);
+		}
+		cur = cur->next;
+	}
+}
