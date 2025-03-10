430a747f882e4efdc18b77fe6d829b7dca81b29b
azinchen <azinchen@student.hive.fi>
2025-03-10 18:01:37 +0200

    96 reds again wow

--- src/cleaners/arr_cleaning.c
+++ src/cleaners/arr_cleaning.c
@@ -68,6 +68,11 @@ void	cleanup_after_execution(t_pipe *p)
 {
 	//close_pipes(p->num_cmds, p->fd);
     wait_for_children(p->num_cmds, p->last_pid, p->ms);
+	if (p->pids)
+	{
+		free(p->pids);
+		p->pids = NULL;
+	}
     cleanup_heredocs(p->ms->heredoc_files);
     p->ms->heredoc_files = malloc(sizeof(char *) * 100);
     ft_memset(p->ms->heredoc_files, 0, sizeof(char *) * 100);
