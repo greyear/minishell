9d2e1cefbeb005ac56318632f8f637da4719d68f
Seela Salorinta <44392065+SeelaSalorinta@users.noreply.github.com>
2025-03-10 19:16:25 +0200

    Seela, 10.03., fixed test cases with . or .. as cmds and export/unset error handling with flags, fixed leaks  (#45)
    * addedcheck for one plus/minus ahead of number
    
    * nothing
    
    * shortened error functions
    
    * shortened functions
    
    * shortened functions
    
    * divided functions to shorter ones
    
    * removed empty files
    
    * removed empty files
    
    * return exit_code correctly now
    
    * fixed error with just using cd
    
    * fixed error with too many args with cd
    
    * fixed some tester cases
    
    * error handling if PATH empty
    
    * error handling if file exists but no permissions
    
    * file handling
    
    * file handling
    
    * cd with too many args
    
    * functions cleaned adn shortened
    
    * restructuring execution
    
    * restructuring execution
    
    * pulling
    
    * closing fd in parent after execution
    
    * closing fd in parent after execution
    
    * closing fds after execution
    
    * pprinting with ft_putstr_fd
    
    * updating SHLVL in envp/exported
    
    * shlvl update with negative number
    
    * changed file names
    
    * refractoring
    
    * adding briefs
    
    * adding briefs
    
    * added briefs
    
    * changed function name
    
    * fixed leak in cd ~
    
    * brief
    
    * malloc checks
    
    * changed file names
    
    * minor fixes
    
    * added rl_clear_history
    
    * malloc checks
    
    * cd ~ leak fixed
    
    * malloc checks
    
    * fixed with 46094 and /bin/bashkey
    
    * rmvd unnecessary functions
    
    * handling $ expansion in heredocs
    
    * fixed leak with zshkey
    
    * cleaning code
    
    * fixed cases with . as cmds
    
    * handling unset and export with flags
    
    ---------
    
    Co-authored-by: Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
    Co-authored-by: Anya Zinchenko <63639724+greyear@users.noreply.github.com>
    Co-authored-by: Seela Salorinta <ssalorin@c3r3p1.hive.fi>

---
+++ src/execution/cmd_checks.c
@@ -0,0 +1,103 @@
+#include "../../include/minishell.h"
+
+/**
+ * @brief Checks whether the command is a directory or a file and attempts to execute it.
+ * 
+ * This function first checks if the provided command is a directory. If it is, an error is printed
+ * and the process exits with status 126. If the command is a file, it checks if the file exists and
+ * is executable. If both conditions are met, the command is executed using `execve`. If the file is
+ * not executable, a permission error is printed, and the process exits with status 126. If the file
+ * does not exist, an error message is printed, and the process exits with status 127.
+ * 
+ * @param envp An array of environment variables used for the `execve` function.
+ * @param cmds An array of strings where `cmds[0]` is the command to be checked and executed.
+ * 
+ * @return This function does not return; it either executes the command or exits the process with
+ *         an appropriate error status.
+ */
+
+void	check_if_dir_or_file(char **envp, char **cmds)
+{
+    DIR *dir;
+	
+	dir = opendir(cmds[0]);
+    if (dir)
+    {
+        closedir(dir);
+        print_cmd_error(cmds[0], IS_DIR);
+        exit(126);
+    }
+    if (access(cmds[0], F_OK) == 0)
+    {
+        if (access(cmds[0], X_OK) == 0)
+            execve(cmds[0], cmds, envp);
+        else
+        {
+			print_cmd_error(cmds[0], PERM_DEN);
+            exit(126);
+        }
+    }
+	print_cmd_error(cmds[0], NO_FILE_OR_DIR);
+    exit(127);
+}
+
+/**
+ * @brief Checks if the command is only a dot (`.`) or starts with multiple dots.
+ *
+ * This function checks if the first command in `cmds` consists only of dots (`.`).
+ * If the condition is met, an error message is printed, and the program exits with a status code of 127, indicating
+ * a command not found error.
+ * 
+ * The function iterates over the characters of the command, ensuring there are no valid commands beyond the dots.
+ * 
+ * @param cmds A pointer to the array of command strings, with `cmds[0]` being the command to check.
+ *
+ * @return None. The function does not return a value. If the condition is met, it prints an error and exits the program.
+ *
+ */
+
+void    check_if_dot(char **cmds)
+{
+    int     i;
+
+    i = 0;
+    while (cmds[0][i] && cmds[0][i] == '.')
+        i++;
+    if (cmds[0][i] == '\0')
+    {
+        print_cmd_error(cmds[0], NO_CMD);
+        exit(127);
+    }
+}
+
+/**
+ * @brief Checks if a file exists and is executable, then attempts to execute it.
+ * 
+ * This function checks if the provided command (`cmds[0]`) exists and is executable using `access`. If the
+ * command exists and is executable, it is executed with `execve`. If the file exists but is not executable,
+ * a permission denied error is printed, and the process exits with status 126. If the file does not exist,
+ * an error indicating that the command was not found is printed, and the process exits with status 127.
+ * 
+ * @param envp An array of environment variables used for the `execve` function.
+ * @param cmds An array of strings where `cmds[0]` is the command to be checked and executed.
+ * 
+ * @return This function does not return; it either executes the command or exits the process with an appropriate
+ *         error status.
+ */
+
+
+void    check_if_file(char **envp, char **cmds)
+{
+    if (access(cmds[0], F_OK) == 0)
+    {
+        if (access(cmds[0], X_OK) == 0)
+            execve(cmds[0], cmds, envp);
+        else
+        {
+			print_cmd_error(cmds[0], PERM_DEN);
+            exit(126);
+        }
+    }
+    print_cmd_error(cmds[0], NO_CMD);
+    exit(127);
+}
