6bc20ac44e6f7da17b9f7a3061c619fbf5f9005d
Seela Salorinta <44392065+SeelaSalorinta@users.noreply.github.com>
2025-03-17 13:35:00 +0200

    Seela, 17.03., signals, heredoc limit, malloc err function (#51)
    * addedcheck for one plus/minus ahead of number
    
    * nothing
    
    * shortened error functions
    
    * shortened functions
    
    * shortened functions
    
    * divided functions to shorter ones
    
    * removed empty files
    
    * removed empty files
    
    * return exit_code correctly now
    
    * fixed error with just using cd
    
    * fixed error with too many args with cd
    
    * fixed some tester cases
    
    * error handling if PATH empty
    
    * error handling if file exists but no permissions
    
    * file handling
    
    * file handling
    
    * cd with too many args
    
    * functions cleaned adn shortened
    
    * restructuring execution
    
    * restructuring execution
    
    * pulling
    
    * closing fd in parent after execution
    
    * closing fd in parent after execution
    
    * closing fds after execution
    
    * pprinting with ft_putstr_fd
    
    * updating SHLVL in envp/exported
    
    * shlvl update with negative number
    
    * changed file names
    
    * refractoring
    
    * adding briefs
    
    * adding briefs
    
    * added briefs
    
    * changed function name
    
    * fixed leak in cd ~
    
    * brief
    
    * malloc checks
    
    * changed file names
    
    * minor fixes
    
    * added rl_clear_history
    
    * malloc checks
    
    * cd ~ leak fixed
    
    * malloc checks
    
    * fixed with 46094 and /bin/bashkey
    
    * rmvd unnecessary functions
    
    * handling $ expansion in heredocs
    
    * fixed leak with zshkey
    
    * cleaning code
    
    * fixed cases with . as cmds
    
    * handling unset and export with flags
    
    * cleaning execution little and making new files
    
    * fixed one error cde
    
    * handling heredocs differently
    
    * heredoc divison, briefs
    
    * parsing hell 16
    
    * heredc files deleted now correctly
    
    * cmd error handling
    
    * executon errors
    
    * dividing main into smaller functions
    
    * signals in heredocs
    
    * fixed ddouble free
    
    * cntrl c
    
    * signal handling
    
    * signal handling
    
    * nothing
    
    * custome tester added
    
    * signal handling
    
    * cleaned function
    
    * checks if envp is Null when running program
    
    * added heredoc limit
    
    * cd - if oldpwd unset
    
    * check before getenv
    
    * malloc checks and cleaning
    
    * return NULL in one case
    
    ---------
    
    Co-authored-by: Seela Salorinta <seelasalorinta@Seela-MacBook-Pro.local>
    Co-authored-by: Anya Zinchenko <63639724+greyear@users.noreply.github.com>
    Co-authored-by: Seela Salorinta <ssalorin@c3r3p1.hive.fi>
    Co-authored-by: Seela Salorinta <ssalorin@c3r1p5.hive.fi>
    Co-authored-by: Seela Salorinta <ssalorin@c2r1p2.hive.fi>

--- include/minishell.h
+++ include/minishell.h
@@ -50,35 +50,35 @@ int				validate_input(char *str);
 int				ft_special(int c);
 
 //BUILTINS
-void	        handle_echo(char **args, t_ms *ms);
-void            handle_env(char **args, t_ms *ms);
-void            print_exported(t_ms *ms);
-void        	add_to_exported(char *key, t_ms *ms);
-void            sort_exported_alphaorder(t_ms *ms);
-void            handle_export(char **args, t_ms *ms);
-void            handle_unset(char **args, t_ms *ms);
-void	        rm_from_env_ex(char ***env, char *name, int len, int flag);
-int             check_env(char *env, char *name, int len, int flag);
-char	        **allocate_temp_env(char **env, int x);
-char	        **copy_map(char **original_map);
-void            print_array(char **a);
-int		        check_if_valid_key(char *name); 
-int		        get_key_length(char *arg);
-char	        *extract_key(char *arg, int len);
-void	        handle_cd(char **args, t_ms *ms);
-void	        update_env_var(t_ms *ms, char *key, char *new_value);
-char	        *build_relative_path(char *target, char *cwd);
-char	        *get_parent_directory(t_ms *ms);
-char	        *get_oldpwd_directory(t_ms *ms);
-char	        *get_home_directory(t_ms *ms, int flag);
-void	        check_pwd(char **array, t_ms *ms);
-char	        *get_env_value(char *key, char **envp);
-void	        check_exit(char	**array, t_ms *ms);
-long long	    convert_to_ll(char *str, int *error);
-char	        *handle_expansion(char *args, t_ms *ms);
-char	        *expand_key(char **envp, char *key, int len, t_ms *ms);
-char	        *duplicate_or_replace(char *entry, char *arg, char *key, int *flag);
-char	        *return_target(t_ms *ms, char *target);
+void			handle_echo(char **args, t_ms *ms);
+void			handle_env(char **args, t_ms *ms);
+void			print_exported(t_ms *ms);
+void			add_to_exported(char *key, t_ms *ms);
+void			sort_exported_alphaorder(t_ms *ms);
+void			handle_export(char **args, t_ms *ms);
+void			handle_unset(char **args, t_ms *ms);
+void			rm_from_env_ex(char ***env, char *name, int len, int flag);
+int				check_env(char *env, char *name, int len, int flag);
+char			**allocate_temp_env(char **env, int x);
+char			**copy_map(char **original_map);
+void			print_array(char **a);
+int				check_if_valid_key(char *name); 
+int				get_key_length(char *arg);
+char			*extract_key(char *arg, int len);
+void			handle_cd(char **args, t_ms *ms);
+void			update_env_var(t_ms *ms, char *key, char *new_value);
+char			*build_relative_path(char *target, char *cwd, t_ms *ms);
+char			*get_parent_directory(t_ms *ms);
+char			*get_oldpwd_directory(t_ms *ms);
+char			*get_home_directory(t_ms *ms, int flag);
+void			check_pwd(char **array, t_ms *ms);
+char			*get_env_value(char *key, char **envp);
+void			check_exit(char	**array, t_ms *ms);
+long long		convert_to_ll(char *str, int *error);
+char			*handle_expansion(char *args, t_ms *ms);
+char			*expand_key(char **envp, char *key, int len, t_ms *ms);
+char			*duplicate_or_replace(char *entry, char *arg, char *key, int *flag);
+char			*return_target(t_ms *ms, char *target);
 
 //Lexer
 t_token			*tokenization(char *str, t_ms *ms);
@@ -118,36 +118,36 @@ t_block			*clean_block_list(t_block **first);
 int				is_builtin(t_cmd *cmd);
 void			handle_builtin(t_cmd *cmd, t_ms *ms, int in_child);
 int				if_children_needed(t_cmd *cmd);
-void            make_one_child(t_cmd *cmd, t_ms *ms);
-void	        execute_command(char **envp, char **cmd);
-void	        make_multiple_childs(int num_cmds, t_cmd *cmds, t_ms *ms);
-void            pipe_process(int prev_pipe, int next_pipe);
-void            redirect_process(int infile, int outfile);
-void            pipe_or_redir(t_cmd *cur, int *pipe_fd, int i, int num_cmds, int cur_fd);
-void	        handle_absolute_or_relative_path(char **envp, char **cmds);
-void            handle_no_path_variable(char **envp, char **cmd);
-void            check_if_dot(char **cmds);
-void            setup_pipes(int *pipe_fd, int i, int num_cmds, int cur_fd);
+void			make_one_child(t_cmd *cmd, t_ms *ms);
+void			execute_command(char **envp, char **cmd);
+void			make_multiple_childs(int num_cmds, t_cmd *cmds, t_ms *ms);
+void			pipe_process(int prev_pipe, int next_pipe);
+void			redirect_process(int infile, int outfile);
+void			pipe_or_redir(t_cmd *cur, int *pipe_fd, int i, int num_cmds, int cur_fd);
+void			handle_absolute_or_relative_path(char **envp, char **cmds);
+void			handle_no_path_variable(char **envp, char **cmd);
+void			check_if_dot(char **cmds);
+void			setup_pipes(int *pipe_fd, int i, int num_cmds, int cur_fd);
 
 
 //Envp
 int				check_list_for_expansions(t_token *first, t_ms *ms);
 int				expand_in_token(t_token *cur, t_ms *ms);
-void            expand_variable(t_ms *ms, char *key, int key_len, char **result);
+void			expand_variable(t_ms *ms, char *key, int key_len, char **result);
 t_envp			*envp_from_list(t_envp *list, char *name);
 t_bool			is_envp_symbol(int c);
 t_bool			is_envp_first_symbol(int c);
 t_ms			*initialize_struct(char **envp);
-void            check_shlvl(t_ms *ms);
+void			check_shlvl(t_ms *ms);
 
 //Redirections
 void			put_infile_fd(t_token *token, t_cmd *cmd);
 void			put_outfile_fd(t_token *token, t_cmd *cmd);
 void			check_access(char *filename, t_oper operation);
-char            *generate_heredoc_filename(int index);
-void	        put_heredoc_fd(t_token *token, t_cmd *cmd, t_ms *ms);
-int             handle_heredoc(t_ms *ms, char *limiter, t_token *token);
-void	        reset_heredocs(t_ms *ms);
+char			*generate_heredoc_filename(int index);
+void			put_heredoc_fd(t_token *token, t_cmd *cmd, t_ms *ms);
+int				handle_heredoc(t_ms *ms, char *limiter, t_token *token);
+void			reset_heredocs(t_ms *ms);
 
 //Reading + history
 t_bool			open_read_history_file(t_ms *ms);
@@ -169,10 +169,11 @@ void			print_unset_error(char **args, int i, t_ms *ms);
 void			print_export_error(t_ms *ms, char *arg);
 void			print_cd_error(char *target_dir);
 void			print_syntax_error(char *text);
-void	        print_numeric_error(char **array);
-void	        print_too_many_args_error(void);
-void	        print_env_error(char **args);
-void	        print_flag_error(char **args);
+void			print_numeric_error(char **array);
+void			print_too_many_args_error(void);
+void			print_env_error(char **args);
+void			print_flag_error(char **args);
+void			print_error(char *error);
 
 //Cleaners
 void			clean_arr(char ***arr);
@@ -180,11 +181,11 @@ void			free_int_array(int **array);
 void			clean_struct(t_ms *ms);
 void			clean_struct_fields(t_ms *ms);
 void			cleanup_heredocs(char **filenames);
-void            close_pipes(int num_cmds, int **pipe_fd);
-void	        free_pids(t_pipe *p);
-void	        close_fds(t_cmd *cmd);
-void            close_all_fds(t_pipe *p, t_ms *ms);
-void            close_file(int file);
-void            close_fds2(int fd1, int fd2);
+void			close_pipes(int num_cmds, int **pipe_fd);
+void			free_pids(t_pipe *p);
+void			close_fds(t_cmd *cmd);
+void			close_all_fds(t_pipe *p, t_ms *ms);
+void			close_file(int file);
+void			close_fds2(int fd1, int fd2);
 
 #endif
