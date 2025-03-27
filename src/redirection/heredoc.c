3134aea23078b738e7e966beed898114d1bef67b
Seela Salorinta <seela.salorinta@gmail.com>
2025-03-27 17:09:20 +0200

    norminette

--- src/redirection/heredoc.c
+++ src/redirection/heredoc.c
@@ -62,7 +62,7 @@ static int	process_line(char **line, char *limiter, t_token *token, t_ms *ms)
 {
 	char		*expanded;
 	t_expand	*exp;
- 
+
 	exp = exp_init(ms);
 	if (ft_strcmp(*line, limiter) == 0)
 	{
